4349f78dfc5696f36fa23aa74bc2171adca786da
AlzyWelzy <welzyalzy@gmail.com>
2023-03-11 11:34:11 +0530

    made the code better

--- old_main.py
+++ old_main.py
@@ -1,34 +1,34 @@
 import random
 from typing import List, Tuple
 
+COLORS = ["R", "G", "B", "B", "W", "S"]
+TRIES = 10
+CODE_LENGTH = 4
+
 
 class Game:
-    COLORS = ["R", "G", "B", "B", "W", "S"]
-    TRIES = 10
-    CODE_LENGTH = 4
-
     def __init__(self):
         self.code = self.generate_code()
 
-    @classmethod
-    def generate_code(cls) -> List[str]:
+    @staticmethod
+    def generate_code() -> List[str]:
         code = []
-        for _ in range(cls.CODE_LENGTH):
-            color = random.choice(cls.COLORS)
+        for _ in range(CODE_LENGTH):
+            color = random.choice(COLORS)
             code.append(color)
         # print(code)
         return code
 
-    @classmethod
-    def guess_code(cls) -> List[str]:
+    @staticmethod
+    def guess_code() -> List[str]:
         while True:
             guess = input("Guess: ").upper().split(" ")
 
-            if len(guess) != cls.CODE_LENGTH:
-                print(f"You must enter {cls.CODE_LENGTH} colors.")
+            if len(guess) != CODE_LENGTH:
+                print(f"YOU MUST GET {CODE_LENGTH} COLORS.")
                 continue
 
-            invalid_colors = [color for color in guess if color not in cls.COLORS]
+            invalid_colors = [color for color in guess if color not in COLORS]
             if invalid_colors:
                 print(f"Invalid colors: {invalid_colors}. Try again.")
                 continue
@@ -49,51 +49,42 @@ class Game:
         for guess_color, real_color in zip(guess, real_code):
             if guess_color == real_color:
                 correct_pos += 1
-                color_counts[real_color] -= 1
+                color_counts[guess_color] -= 1
 
         for guess_color, real_color in zip(guess, real_code):
-            if (
-                guess_color in color_counts
-                and color_counts[guess_color] > 0
-                and guess_color != real_color
-            ):
+            if guess_color in color_counts and color_counts[guess_color] > 0:
                 incorrect_pos += 1
                 color_counts[guess_color] -= 1
 
         return correct_pos, incorrect_pos
 
-    def play(self) -> None:
-        print(
-            f"WELCOME TO MASTERMIND, you have {self.TRIES} tries to guess the code..."
-        )
-        print("The valid colors are", *self.COLORS)
+    def play(self) -> bool:
+        print(f"WELCOME TO mastermind, you have {TRIES} to guess the code...")
+        print("The valid colors are", *COLORS)
 
-        for attempt in range(1, self.TRIES + 1):
+        for attempt in range(1, TRIES + 1):
             guess = self.guess_code()
             correct_pos, incorrect_pos = self.check_code(guess, self.code)
 
-            if correct_pos == self.CODE_LENGTH:
+            if correct_pos == CODE_LENGTH:
                 print(f"You guessed the code in {attempt} tries!")
-                break
+                return True
 
             print(
                 f"Correct Positions: {correct_pos} | Incorrect Positions: {incorrect_pos}"
             )
 
-        else:
-            print("You ran out of tries, the code was:", *self.code)
-
-    def __str__(self):
-        return f"Code: {self.code}"
+        print("You ran out of tries, the code was:", *self.code)
+        return False
 
 
 def main() -> None:
     while True:
         game = Game()
-        game.play()
-        play_again = input("Would you like to play again? (y/n): ").lower()
+        success = game.play()
+        again = input("Would you love to play gain? (y/n): ")
 
-        if play_again == "n":
+        if again.lower() == "n":
             break
 
 
